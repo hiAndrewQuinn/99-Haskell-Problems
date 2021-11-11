4a2dfd6983fd2a54feda3827454dde5a10721bdb
Andrew Quinn <53230903+AndrewQuinn2020@users.noreply.github.com>
2021-11-11 15:01:52 +0200

    began p3

---
+++ p3.hs
@@ -0,0 +1,21 @@
+-- Problem 3
+
+-- (*) Find the K'th element of a list. The first element in the list is number 1.
+
+-- Example:
+
+-- * (element-at '(a b c d e) 3)
+-- c
+
+-- Example in Haskell:
+
+-- > elementAt [1,2,3] 2
+-- 2
+-- > elementAt "haskell" 5
+-- 'e'
+
+module Problem3 where
+
+main :: IO ()
+main = do
+  putStrLn "Problem 3\n=====\n"
